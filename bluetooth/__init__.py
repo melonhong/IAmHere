d6b70e174cea46a7aa3e6529508e4c45d4b936c9
seoyoon <20221174@sungshin.ac.kr>
2025-04-26 19:43:43 +0100

    IAM-25 feat: 모듈 추가

--- bluetooth/__init__.py
+++ bluetooth/__init__.py
@@ -1,5 +1,12 @@
 from .bluetooth import (
     scan_bluetooth_devices,
+    scan_off,
     add_device,
     get_mac_addresses_by_user_ids
+)
+from .lecturer_connection import (
+    pair_device,
+    connect_device,
+    monitor_connection,
+    is_connected
 )
