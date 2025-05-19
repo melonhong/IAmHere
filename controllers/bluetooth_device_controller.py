d55ddf99280bbe7fe5db4d48b54a384171a17b28
20221174 <20221174@sungshin.ac.kr>
2025-05-19 21:28:15 +0900

    feat: get_mac_addr 추가

--- controllers/bluetooth_device_controller.py
+++ controllers/bluetooth_device_controller.py
@@ -1,8 +1,13 @@
-from models import BluetoothDeviceDAO
+from models import BluetoothDeviceDAO, LectureDAO
 
 class BluetoothDeviceController:
     def __init__(self):
         self.bluetooth_dao = BluetoothDeviceDAO()
+        self.lecture_dao = LectureDAO()
 
     def register_enrollment(self, user_id, mac_addr, name):
         return bool(self.bluetooth_dao.add_bluetooth_device(user_id, mac_addr, name))
+    
+    def get_mac_addr(self, lecture_id):
+        professor_id = self.lecture_dao.get_professor_by_id(lecture_id)
+        return self.bluetooth_dao.get_mac_by_user_id(professor_id)
