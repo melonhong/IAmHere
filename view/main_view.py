99a35816318121d2387e70f08cfe8fe63562c46e
20221174 <20221174@sungshin.ac.kr>
2025-05-19 17:24:45 +0900

    feat: 수강신청, 블루투스, 지문 등록 뷰 불러오기

--- view/main_view.py
+++ view/main_view.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 from view.user_register import open_user_register_window
 from view.lecture_register import open_lecture_register_window
+from view.enrollment_register import open_enrollment_register_window
+from view.bluetooth_device_register import open_bluetooth_device_register_window
+from view.fingerprint_register import open_fingerprint_register_window
 
 def run_gui():
     root = tk.Tk()
@@ -11,6 +14,9 @@ def run_gui():
 
     tk.Button(root, text="사용자 등록", command=lambda: open_user_register_window(root), width=30).pack(pady=5)
     tk.Button(root, text="강의 등록", command=lambda: open_lecture_register_window(root), width=30).pack(pady=5)
+    tk.Button(root, text="수강신청", command=lambda: open_enrollment_register_window(root), width=30).pack(pady=5)
+    tk.Button(root, text="블루투스 기기 등록", command=lambda: open_bluetooth_device_register_window(root), width=30).pack(pady=5)
+    tk.Button(root, text="지문 등록", command=lambda: open_fingerprint_register_window(root), width=30).pack(pady=5)
     tk.Button(root, text="종료", command=root.quit, width=30).pack(pady=5)
 
     root.mainloop()
