71266a468f992b0292844fb38c699cdb6bb70149
20221174 <20221174@sungshin.ac.kr>
2025-05-28 21:28:27 +0900

    fix

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 from models import initialize_database
-from viewss import main_views  # gui/gui_main.py 에 run_gui() 함수가 있다고 가정
+from views import main_view
 
 def main():
     # DB 초기화
     initialize_database()
 
     # GUI 실행
-    main_views.run_gui()
+    main_view.run_gui()
 
 if __name__ == "__main__":
     main()
