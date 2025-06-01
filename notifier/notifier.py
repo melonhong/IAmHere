e9d1e0d3f2b3591d4752f2abb79c79a5ac69c7b4
20221174 <20221174@sungshin.ac.kr>
2025-06-01 13:39:42 +0900

    refactor: 코드 가독성 개선

--- notifier/notifier.py
+++ notifier/notifier.py
@@ -4,9 +4,7 @@ from config import NOTIFY_API_URL
 def send_check(name: str, lecture_title: str):
     if not NOTIFY_API_URL:
         raise ValueError("환경변수 NOTIFY_API_URL이 설정되지 않았습니다.")
-
     params = {"name": name, "lecture_title": lecture_title}  # 쿼리 파라미터로 보낼 데이터
-
     try:
         response = requests.get(NOTIFY_API_URL + "/second-check", params=params)
         response.raise_for_status()
@@ -17,9 +15,7 @@ def send_check(name: str, lecture_title: str):
 def send_result(name: str, result: bool):
     if not NOTIFY_API_URL:
         raise ValueError("환경변수 NOTIFY_API_URL이 설정되지 않았습니다.")
-
     params = {"name": name, "result": result}  # 쿼리 파라미터로 보낼 데이터
-
     try:
         response = requests.get(NOTIFY_API_URL + "/second-result", params=params)
         response.raise_for_status()
